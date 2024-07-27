f4a2cbed857fac1d2efdd7023ca7c95b267b068f
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2024-07-27 17:56:11 +0000

    ✨ Add registration and login functionality to airline_tickets app

--- app/airline_tickets/views.py
+++ app/airline_tickets/views.py
@@ -1,12 +1,15 @@
 from airline_tickets.dto.flight_search_dto import AirportDto
+from airline_tickets.forms import LoginForm, RegisterAccountForm
 from airline_tickets.models import Airport
-from django.http import HttpResponse
-from django.shortcuts import render
+from airline_tickets.services.account_service import register_account_data
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect, render
 
 # Create your views here.
 
 
-def index(request):
+def top(request: HttpRequest) -> HttpResponse:
     available_airports = Airport.objects.all()
     airport_list = [
         AirportDto(
@@ -18,28 +21,104 @@ def index(request):
         for airport in available_airports
     ]
 
-    login = request.session.get("authorized_user_login", None)
+    username = request.user.get_username() if request.user.is_authenticated else ""
 
-    context = {"available_airports": airport_list, "user_login": login}
+    return render(
+        request,
+        "top.html",
+        {
+            "available_airports": airport_list,
+            "username": username,
+        },
+    )
 
-    return render(request, "index.html", context)
 
-
-def search(request):
+def search(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Search page")
 
 
-def register_account_page(request):
-    return HttpResponse("Register account page")
+def register_account_page(
+    request: HttpRequest,
+    registered_ok: bool = False,
+    error_msg: str = "",
+    register_form: RegisterAccountForm = RegisterAccountForm(),
+) -> HttpResponse:
+    if request.user.is_authenticated:
+        return redirect("top", permanent=True)
+
+    if request.method == "POST":
+        return do_register_account(request)
+
+    return render(
+        request,
+        "register_account.html",
+        {
+            "registered_ok": registered_ok,
+            "error_msg": error_msg,
+            "form": register_form,
+        },
+    )
 
 
-def register_account(request):
-    return HttpResponse("Register account")
+def do_register_account(request: HttpRequest) -> HttpResponse:
+    form = RegisterAccountForm(request.POST)
+
+    try:
+        username, password = register_account_data(form)
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            raise RuntimeError("Failed to create user.")
+
+    except RuntimeError as e:
+        request.method = "GET"
+        return register_account_page(request, e is None, str(e), register_form=form)
+
+    login(request, user)
+    return redirect("top", permanent=True)
 
 
-def login_page(request):
-    return render(request, "login.html", {})
+def login_page(
+    request: HttpRequest,
+    error_msg: str = "",
+    login_form: LoginForm = LoginForm(),
+) -> HttpResponse:
+    if request.user.is_authenticated:
+        return redirect("top", permanent=True)
+
+    if request.method == "POST":
+        return do_login(request)
+
+    return render(
+        request,
+        "login.html",
+        {
+            "error_msg": error_msg,
+            "form": login_form,
+        },
+    )
 
 
-def login(request):
-    return HttpResponse("Login")
+def do_login(request: HttpRequest) -> HttpResponse:
+    form = LoginForm(request.POST)
+    try:
+        if not form.is_valid():
+            raise RuntimeError()
+
+        username = form.cleaned_data["username"]
+        password = form.cleaned_data["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            raise RuntimeError()
+
+    except RuntimeError:
+        error = "Invalid username or password."
+        request.method = "GET"
+        return login_page(request, error, form)
+
+    login(request, user)
+    return redirect("top", permanent=True)
+
+
+def do_logout(request: HttpRequest):
+    logout(request)
+    return redirect("login", permanent=True)
