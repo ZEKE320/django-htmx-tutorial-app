3e6ef03585bac545cf6711ac0359b3b1871e2a3f
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2024-07-27 17:54:42 +0000

    ✨ Add registration functionality to airline_tickets app

---
+++ app/airline_tickets/services/account_service.py
@@ -0,0 +1,20 @@
+from airline_tickets.forms import RegisterAccountForm
+from airline_tickets.models import CustomerAccount
+
+
+def register_account_data(form: RegisterAccountForm):
+    if not form.is_valid():
+        raise RuntimeError(f"Error: {str(form.errors)}")
+
+    username = form.cleaned_data["reg_username"]
+    password = form.cleaned_data["reg_password"]
+    password_again = form.cleaned_data["reg_password_again"]
+
+    if len(CustomerAccount.objects.filter(username=username)) > 0:
+        raise RuntimeError(f"A user with the name {username} already exists.")
+
+    if password != password_again:
+        raise RuntimeError("Passwords do not match.")
+
+    CustomerAccount.objects.create_user(username=username, password=password)
+    return username, password
