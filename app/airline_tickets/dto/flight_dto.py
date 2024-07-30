80c826ec9982a5232821a483e2f3d3d3ddcc2a8e
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2024-07-30 05:20:33 +0000

    🚚 Rename flight_search_dto.py to flight_dto.py and add UUID4 field to AirportDto

--- app/airline_tickets/dto/flight_dto.py
+++ app/airline_tickets/dto/flight_dto.py
@@ -1,8 +1,10 @@
-from pydantic import BaseModel
+from pydantic import UUID4, BaseModel
 
 
 class AirportDto(BaseModel):
+    id: UUID4
     name: str
     country: str
     prefecture: str
     iata_code: str
+
