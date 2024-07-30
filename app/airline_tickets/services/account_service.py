73ac33b383a4f6a20a71a8352b49a39ccee0641a
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2024-07-30 06:00:00 +0000

    ♻️ Refactor account registration to check for existing username
    This commit refactors the `register_account_data` function in the `account_service.py` file. It modifies the logic to check for an existing username before creating a new customer account. If a user with the same username already exists, a `RuntimeError` is raised. This change improves the reliability of the account registration process and prevents duplicate usernames.

--- app/airline_tickets/services/account_service.py
+++ app/airline_tickets/services/account_service.py
@@ -1,22 +1,21 @@
-from airline_tickets.forms import RegisterAccountForm
+from airline_tickets.forms.account_form import RegisterAccountForm
 from airline_tickets.models import CustomerAccount
 from django.db import transaction
 
 
 @transaction.atomic
 def register_account_data(form: RegisterAccountForm):
-    if not form.is_valid():
-        raise RuntimeError(f"Error: {str(form.errors)}")
-
     username = form.cleaned_data["reg_username"]
     password = form.cleaned_data["reg_password"]
     password_again = form.cleaned_data["reg_password_again"]
 
-    if len(CustomerAccount.objects.filter(username=username)) > 0:
-        raise RuntimeError(f"A user with the name {username} already exists.")
-
     if password != password_again:
         raise RuntimeError("Passwords do not match.")
+
+    customer_accounts = CustomerAccount.objects.filter(username=username)
+
+    if customer_accounts.exists():
+        raise RuntimeError(f"The user with the name '{username}' already exists.")
 
     CustomerAccount.objects.create_user(username=username, password=password)
     return username, password
